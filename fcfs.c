5d818857fd560df80808f06f861e4ff978433fe3
surabhishetty22 <116055119+surabhishetty22@users.noreply.github.com>
2023-07-13 11:36:36 +0530

    Update fcfs.c

--- fcfs.c
+++ fcfs.c
@@ -1,40 +1,4 @@
 #include<stdio.h>
-int main(){
-    /*output
-    Enter the number of processes: 4
-    Enter the arrival time for process 1:2
-    Enter the Burst time for processes 1:1
-    Enter the arrival time for process 2:1
-    Enter the Burst time for processes 2:5
-    Enter the arrival time for process 3:4
-    Enter the Burst time for processes 3:1
-    Enter the arrival time for process 4:0
-    Enter the Burst time for processes 4:6
-    FCFS agorithm
-    Processes       AT      BT      WT      TAT     CT      RT
-    4               0       6       0       6       6       0
-    2               1       5       5       10      11      5
-    1               2       1       9       10      12      9
-    3               4       1       8       9       13      8
-    AVG waiting time: 5.500000
-    AVG turnaround time: 8.750000
-    Total Idle time: 0
-    */
-    int n=0;
-    printf("Enter the number of processes: ");
-    scanf("%d",&n);
-    int processes[n],burst_time[n],arrival_time[n];
-    for (int i = 0; i < n; i++)
-    {
-        printf("Enter the arrival time for process %d:",i+1);
-        scanf("%d",&arrival_time[i]);
-        printf("Enter the Burst time for processes %d:",i+1);
-        scanf("%d",&burst_time[i]);
-        processes[i]=i+1;
-    }
-    fcfs(processes,n,burst_time,arrival_time);
-}
-
 void printer(int processes[],int n,int burst_time[],int arrival_time[])
 {
     int WT[n],TAT[n],total_WT=0,total_TAT=0,completion_time[n],idle=0,RT[n];
@@ -110,3 +74,37 @@ void fcfs(int processes[],int n,int burst_time[],int arrival_time[])
     printf("FCFS agorithm\n");
     printer(processes,n,burst_time,arrival_time);
 }
+void main()
+{
+    int n=0;
+    printf("Enter the number of processes: ");
+    scanf("%d",&n);
+    int processes[n],burst_time[n],arrival_time[n];
+    for (int i = 0; i < n; i++)
+    {
+        printf("Enter the arrival time for process %d:",i+1);
+        scanf("%d",&arrival_time[i]);
+        printf("Enter the Burst time for processes %d:",i+1);
+        scanf("%d",&burst_time[i]);
+        processes[i]=i+1;
+    }
+    fcfs(processes,n,burst_time,arrival_time);
+}
+//* Enter the arrival time for process 1:2
+Enter the Burst time for processes 1:1
+Enter the arrival time for process 2:1
+Enter the Burst time for processes 2:5
+Enter the arrival time for process 3:4
+Enter the Burst time for processes 3:1
+Enter the arrival time for process 4:0
+Enter the Burst time for processes 4:6
+FCFS agorithm
+Processes       AT      BT      WT      TAT     CT      RT
+4               0       6       0       6       6       0
+2               1       5       5       10      11      5
+1               2       1       9       10      12      9
+3               4       1       8       9       13      8
+AVG waiting time: 5.500000
+AVG turnaround time: 8.750000
+Total Idle time: 0
+*//
